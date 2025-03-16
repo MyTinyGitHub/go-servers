b6e8820723a921bf7580b1e70bb1637eb7a1bcca
Dominik Pall <kurdax@Kurdax>
2025-03-16 14:08:57 +0100

    add middleware to cout number of visits

--- main.go
+++ main.go
@@ -1,6 +1,22 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+  fileserverHits atomic.Int32
+} 
+
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.HandlerFunc {
+  return func(res http.ResponseWriter, req *http.Request) {
+    cfg.fileserverHits.Add(1)
+    next.ServeHTTP(res, req)
+  }
+}
 
 func serveHTTPHealthz(res http.ResponseWriter, req *http.Request) {
   res.WriteHeader(200)
@@ -8,16 +24,36 @@ func serveHTTPHealthz(res http.ResponseWriter, req *http.Request) {
   res.Write([]byte("OK"))
 }
 
+func (cfg *apiConfig) serveMetrics(res http.ResponseWriter, req *http.Request) {
+  res.WriteHeader(200)
+  res.Header().Add("Content-Type", "text/plain")
+
+  value := cfg.fileserverHits.Load()
+  fmt.Fprintf(res, "Hits: %v", value)
+}
+
+func (cfg *apiConfig) resetMetrics(res http.ResponseWriter, req *http.Request) {
+  res.WriteHeader(200)
+  res.Header().Add("Content-Type", "text/plain")
+  cfg.fileserverHits.Store(0)
+}
 
 func main() {
   mux := http.NewServeMux()
+
+  var conf apiConfig 
+
   server := &http.Server{
     Handler: mux,
     Addr: ":8080",
   }
 
-  mux.Handle("/app/", http.FileServer(http.Dir(".")))
+  handler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
+  
+  mux.HandleFunc("/app/", conf.middlewareMetricsInc(handler))
   mux.HandleFunc("/healthz", serveHTTPHealthz)
+  mux.HandleFunc("/metrics", conf.serveMetrics)
+  mux.HandleFunc("/reset", conf.resetMetrics)
   
   server.ListenAndServe()
 }
